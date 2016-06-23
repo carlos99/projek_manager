0df6f462ef741635ac943fc6ebd7927d7845bf3e
Carlos Orellana <carlos@dccolorweb.com>
2016-06-23 00:00:39 -0600

    Creating the comments model and adding the necesary relathionship

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :project
-
+  has_many :comments
   validates :title, presence: true
   validates :title, length: { minimum: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160619203343) do
+ActiveRecord::Schema.define(version: 20160620035821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "body",       null: false
+    t.integer  "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["task_id"], name: "index_comments_on_task_id", using: :btree
 
   create_table "projects", force: :cascade do |t|
     t.string   "name",       null: false
@@ -33,5 +44,6 @@ ActiveRecord::Schema.define(version: 20160619203343) do
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
 
+  add_foreign_key "comments", "tasks"
   add_foreign_key "tasks", "projects"
 end
