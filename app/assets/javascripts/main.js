cb745c0ef761229940b37f6921298826d9d0fead
Carlos Orellana <carlos@dccolorweb.com>
2016-06-25 06:16:03 -0600

    Adding checkbox functionality using ajax. Now you can mark a task as done from the project detail view

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+$(function() {
+  $(".task_done").on("change", function() {
+    var _this = $(this);
+    $.post("/tasks/" + $(this).data("id") + "/toggle", { "_method": "patch" }, function() {
+    });
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  root 'projects#index'
+
   resources :tasks do
     resources :comments, only: [:create]
+    member do
+      patch :toggle
+    end
   end
   resources :projects
 
